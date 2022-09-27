16f5c55ad0cd9ec20d0fa3d9930dd906fcb544f1
Allo <51295326+ChaeJiHyeon@users.noreply.github.com>
2022-09-27 22:10:08 +0900

    Create divisor.java

---
+++ divisor.java
@@ -0,0 +1,22 @@
+public class divisor {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n <= 1) {
+                answer = i;
+            } else if (n % i == 0) {
+                answer += i;
+                if (n / i != i) {
+                    answer += ((int) (n / i));
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        divisor d = new divisor();
+    }
+}
