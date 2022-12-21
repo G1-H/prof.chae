2ff3759b4584679948e6ac3a45bbffab7df81f19
Allo <51295326+ChaeJiHyeon@users.noreply.github.com>
2022-12-21 11:07:11 +0900

    1221
    이상한 문자 다른 함수 포함

--- lv1/Strange.java
+++ lv1/Strange.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import javax.swing.event.SwingPropertyChangeSupport;
+
 class Strange {
     public String solution(String st) {
         int cnt = 0;
@@ -19,19 +21,52 @@ class Strange {
         return answer;
     }
 
+    public String solu(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] temp1 = s.split("");
+        int index = 0;
+
+        System.out.println("temp1:" + Arrays.toString(temp1));
+        for (int i = 0; i < temp1.length; i++) {
+            System.out.println(temp1[i] + ".");
+            if (temp1[i].equals(" ")) {
+                sb.append(" ");
+                index = 0;
+            } else {
+                if (index % 2 == 0) {
+                    sb.append(temp1[i].toUpperCase());
+                    index++;
+                } else {
+                    sb.append(temp1[i].toLowerCase());
+                    index++;
+                }
+            }
+        }
+        return sb.toString().substring(0, sb.length());
+
+    }
+
     public static void main(String[] args) {
         Strange str = new Strange();
 
         String s = "try hello world";
-        System.out.println(str.solution(s));
+        System.out.println(str.solution(s) + ".");
+        System.out.println(str.solu(s) + ".");
+
+        s = "try allo hello world";
+        System.out.println(str.solution(s) + ".");
+        System.out.println(str.solu(s) + ".");
 
         s = "tRy hello wOrld";
-        System.out.println(str.solution(s));
+        System.out.println(str.solution(s) + ".");
+        System.out.println(str.solu(s) + ".");
 
         s = "tRy            hello wOrld";
-        System.out.println(str.solution(s));
+        System.out.println(str.solution(s) + ".");
+        System.out.println(str.solu(s) + ".");
 
         s = " tRy            hello wOrld   ";
-        System.out.println(str.solution(s));
+        System.out.println(str.solution(s) + ".");
+        System.out.println(str.solu(s) + ".");
     }
 }
